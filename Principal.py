61d59803fdcb6eafb334e5cd3f8464ccb0eb1b18
GabrielMonteiroGuerra <gabrielmonteiroguerra@hotmail.com>
2021-10-26 12:34:50 -0300

    Primeiros passos

---
+++ Principal.py
@@ -0,0 +1,19 @@
+print("Olá, Mundo!") #1 
+
+num = int(input('Digite um número: ')) #Inserindo dados via console #2
+print("O número informado foi:",num) #imprimindo o valor digitado #2
+
+#---------------------------------------------------------------------
+
+num1 = int(input('Digite o primeiro número:')) #Digitando o primeiro número #3
+num2 = int(input('Digite o segundo número:')) #Digitando o segundo número #3
+print('Soma = ',num1 + num2) #Imprimindo a soma dos dois valores
+
+#-------------------------------------------------------------------------------------
+nome = str(input('Digite o seu nome')) #Entrando com dados tipo String
+nota1 = float(input('Digite a primeira nota:')) #Nota 1
+nota2 = float(input('Digite a primeira nota:')) #Nota 2
+nota3 = float(input('Digite a primeira nota:')) #Nota 3
+nota4 = float(input('Digite a primeira nota:')) #Nota 4
+media = (nota1 + nota2 + nota3 + nota4)/4 #Fazendo a média das notas
+print('A média do', nome, 'é:',media) #Imprimindo a média
